3ac55e8bab9f9b4f765dcec22543779c4d33646f
Samarjeet shelke <59022780+Samarjeetshelke@users.noreply.github.com>
2021-10-15 07:49:59 +0530

    Add files via upload

---
+++ ifelsestatement.java
@@ -0,0 +1,11 @@
+class ifelsestatement{
+    public static void main(String args[]){
+        int age = 15;
+	if(age > 18){
+		System.out.println("Your age is "+age +".You can voat");
+	}
+	else{
+		System.out.println("Your age is "+age +".You can not  voat");
+	}
+    }
+}
